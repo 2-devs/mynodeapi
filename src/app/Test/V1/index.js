acf4d04e0d4fb9c277548b3e85525516a41ef8a0
Mauro Marssola <mmarssola@verios.com.br>
2020-01-14 11:38:13 -0300

    Removed Test module

--- src/app/Test/V1/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../../../middlewares/auth')
-
-router.get('/', async (req, res) => {
-    return res.status(200).send({ ok: true })
-})
-
-module.exports = app => app.use('/V1/test', router)
